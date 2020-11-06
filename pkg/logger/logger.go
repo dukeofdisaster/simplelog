d5bb2d9adb38855aa3dc901609c02bf9631f7ed0
Parallels <parallels@boxuser@ubuntu.local>
2020-11-05 17:31:33 -0700

    added additional funcs that log to global log.Logger() to avoid passing around logger object everyhwere

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -7,6 +7,8 @@ import (
 )
 // GLOBALS
 var DEFAULT = "/var/log/simplelogger.log"
+var CURRENT_LOGGER *log.Logger = nil
+
 func GetLogger(filepath string) (*log.Logger) {
 	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
 	if err != nil {
@@ -15,7 +17,38 @@ func GetLogger(filepath string) (*log.Logger) {
 	return(log.New(f,"",log.LstdFlags))
 }
 
-/* LOGGING FUNCTINOALITY */
+func SetLogger(filepath string) {
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+    CURRENT_LOGGER = log.New(f,"",log.LstdFlags)
+}
+
+/* Log with global logger after calling SetLogger */
+func Infos(msg string) {
+    if CURRENT_LOGGER !=  nil {
+        CURRENT_LOGGER.Println("INFO: "+msg)
+    } else {
+        panic("Attempted to log to nil logger; did you run logger.SetLogger() ?")
+    }
+}
+func Warns(msg string) {
+    if CURRENT_LOGGER != nil {
+        CURRENT_LOGGER.Println("WARN: "+msg)
+    } else {
+        panic("Attempted to log to nil logger; did you run loger.SetLogger() ?")
+    }
+}
+func Debugs(msg string) {
+    if CURRENT_LOGGER != nil {
+        CURRENT_LOGGER.Println("DEBUG: "+msg)
+    } else {
+        panic("Attempted to log to nil logger; did you run logger.SetLogger() ?")
+    }
+}
+
+/* Log with pointer */
 func Info(someLogger *log.Logger,msg string) {
 	someLogger.Println("INFO: "+msg)
 }
