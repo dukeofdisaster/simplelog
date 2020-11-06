d80925eb31bef89429233ffc32fee5f4744c2e24
Parallels <parallels@boxuser@ubuntu.local>
2020-11-05 17:39:36 -0700

    fix typo, err msg... shit

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,27 +1,28 @@
 package logger
 
 import (
-	"fmt"
-	"os"
-	"log"
+    "fmt"
+    "os"
+    "log"
 )
 // GLOBALS
 var DEFAULT = "/var/log/simplelogger.log"
 var CURRENT_LOGGER *log.Logger = nil
+var ERR_NIL_LOGGER = "Attempted to log to nil logger; did you run logger.SetLogger() ?"
 
 func GetLogger(filepath string) (*log.Logger) {
-	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return(log.New(f,"",log.LstdFlags))
+    f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
+    if err != nil {
+        fmt.Println(err)
+    }
+    return(log.New(f,"",log.LstdFlags))
 }
 
 func SetLogger(filepath string) {
-	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
+    f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
+    if err != nil {
+        fmt.Println(err)
+    }
     CURRENT_LOGGER = log.New(f,"",log.LstdFlags)
 }
 
@@ -30,32 +31,32 @@ func Infos(msg string) {
     if CURRENT_LOGGER !=  nil {
         CURRENT_LOGGER.Println("INFO: "+msg)
     } else {
-        panic("Attempted to log to nil logger; did you run logger.SetLogger() ?")
+        panic(ERR_NIL_LOGGER)
     }
 }
 func Warns(msg string) {
     if CURRENT_LOGGER != nil {
         CURRENT_LOGGER.Println("WARN: "+msg)
     } else {
-        panic("Attempted to log to nil logger; did you run loger.SetLogger() ?")
+        panic(ERR_NIL_LOGGER)
     }
 }
 func Debugs(msg string) {
     if CURRENT_LOGGER != nil {
         CURRENT_LOGGER.Println("DEBUG: "+msg)
     } else {
-        panic("Attempted to log to nil logger; did you run logger.SetLogger() ?")
+        panic(ERR_NIL_LOGGER)
     }
 }
 
 /* Log with pointer */
 func Info(someLogger *log.Logger,msg string) {
-	someLogger.Println("INFO: "+msg)
+    someLogger.Println("INFO: "+msg)
 }
 func Warn(someLogger *log.Logger, msg string) {
-	someLogger.Println("WARN: "+msg)
+    someLogger.Println("WARN: "+msg)
 }
 func Debug(someLogger *log.Logger, msg string) {
-	someLogger.Println("DBUG: "+msg)
+    someLogger.Println("DBUG: "+msg)
 }
 
