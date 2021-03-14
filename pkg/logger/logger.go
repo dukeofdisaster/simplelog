7db213bfba41acd67cb5aff96b7286773f6dfeb6
boxuser <boxuser@localbox.local>
2021-03-14 13:11:55 -0700

    improved error handling

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -19,49 +19,46 @@ var ISO_8601_FMT = "2006-01-02T15:04:05.999Z"
 var DEFAULT = "/var/log/simplelogger.log"
 var CURRENT_LOGGER *log.Logger = nil
 var UTC_LOGGER *LocalWriter = nil
-var ERR_NIL_LOGGER = "Attempted to log to nil logger; did you run logger.SetLogger() ?"
-var ERR_NIL_UTC_LOGGER = "Attempted to log with nil LocalWriterr; did you run logger.SetLoggerUtc() ?"
+var ERR_NIL_LOGGER = "attempted to log to nil logger; did you run logger.SetLogger() ?"
+var ERR_NIL_UTC_LOGGER = "attempted to log with nil LocalWriter; did you run logger.SetLoggerUtc() ?"
 
 // define our own Write method for LocalWriter so we can give our own timestamp
 func (writer *LocalWriter) Write(bytes []byte) (int, error) {
 	f, err := os.OpenFile(writer.DestLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer f.Close()
 	if err != nil {
-		return 1, err
+		return 1, fmt.Errorf("LocalWriter.Write(): %v", err)
 	}
 	ws := io.WriteString
 	msg := string(bytes)
 	return ws(f, fmt.Sprintf(time.Now().UTC().Format(ISO_8601_FMT)+" "+msg))
 }
 
-func GetLogger(filepath string) *log.Logger {
+func GetLogger(filepath string) (*log.Logger, error) {
 	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println(err)
+        return nil, fmt.Errorf("GetLogger(): %v", err)
 	}
-	return (log.New(f, "", log.LstdFlags))
+	return log.New(f, "", log.LstdFlags), nil
 }
 
 func SetLogger(filepath string) error {
 	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return err
+		return fmt.Errorf("SetLogger(): %v", err)
 	}
 	CURRENT_LOGGER = log.New(f, "", log.LstdFlags)
 	return nil
 }
 
-/*
-   Amounts to SetOutput, which doesn't return err in std lib
-*/
 func SetLoggerUtc(filepath string) error {
 	dir := path.Dir(filepath)
 	if dir == "/" || filepath == "/" {
-		return errors.New("Shouldn't write to / ; try a different path")
+		return errors.New("SetLoggerUtc(): wont write to / ; try a different path")
 	}
 	err := syscall.Access(dir, syscall.O_RDWR)
 	if err != nil {
-		return err
+		return fmt.Errorf("SetLoggerUtc(): syscall.Acces(): %s %v", dir, err)
 	}
 	UTC_LOGGER = new(LocalWriter)
 	UTC_LOGGER.DestLog = filepath
@@ -107,25 +104,25 @@ func Err(e error) error {
 }
 
 /* Log with global logger after calling SetLogger */
-func Infos(msg string) {
+func Infos(msg string) error {
 	if CURRENT_LOGGER != nil {
 		CURRENT_LOGGER.Println("INFO: " + msg)
 	} else {
-		panic(ERR_NIL_LOGGER)
+		return errors.New(ERR_NIL_LOGGER)
 	}
 }
-func Warns(msg string) {
+func Warns(msg string) error {
 	if CURRENT_LOGGER != nil {
 		CURRENT_LOGGER.Println("WARN: " + msg)
 	} else {
-		panic(ERR_NIL_LOGGER)
+		return errors.New(ERR_NIL_LOGGER)
 	}
 }
-func Debugs(msg string) {
+func Debugs(msg string) error {
 	if CURRENT_LOGGER != nil {
 		CURRENT_LOGGER.Println("DEBUG: " + msg)
 	} else {
-		panic(ERR_NIL_LOGGER)
+		return errors.New(ERR_NIL_LOGGER)
 	}
 }
 
