8697ae03e75b7381c308431d67523f9bb56a6297
root <root@metalbox.local>
2021-03-14 18:07:53 +0000

    added some tests and path validation

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,18 +1,18 @@
 package logger
 
 import (
-    "fmt"
-    "errors"
-    "io"
-    "log"
-    "os"
-    "path"
-    "syscall"
-    "time"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"syscall"
+	"time"
 )
 
 type LocalWriter struct {
-    DestLog string
+	DestLog string
 }
 
 var ISO_8601_FMT = "2006-01-02T15:04:05.999Z"
@@ -23,126 +23,119 @@ var ERR_NIL_LOGGER = "Attempted to log to nil logger; did you run logger.SetLogg
 var ERR_NIL_UTC_LOGGER = "Attempted to log with nil LocalWriterr; did you run logger.SetLoggerUtc() ?"
 
 // define our own Write method for LocalWriter so we can give our own timestamp
-func (writer *LocalWriter) Write(bytes []byte) (int,error) {
-    f,err := os.OpenFile(writer.DestLog,os.O_APPEND|os.O_CREATE|os.O_WRONLY,0644)
-    defer f.Close()
-    if err != nil {
-        return 1, err
-    }
-    ws := io.WriteString
-    msg := string(bytes)
-    return ws(f,fmt.Sprintf(time.Now().UTC().Format(ISO_8601_FMT) + " " + msg))
+func (writer *LocalWriter) Write(bytes []byte) (int, error) {
+	f, err := os.OpenFile(writer.DestLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	if err != nil {
+		return 1, err
+	}
+	ws := io.WriteString
+	msg := string(bytes)
+	return ws(f, fmt.Sprintf(time.Now().UTC().Format(ISO_8601_FMT)+" "+msg))
 }
 
-func GetLogger(filepath string) (*log.Logger) {
-    f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
-    if err != nil {
-        fmt.Println(err)
-    }
-    return(log.New(f,"",log.LstdFlags))
+func GetLogger(filepath string) *log.Logger {
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return (log.New(f, "", log.LstdFlags))
 }
 
 func SetLogger(filepath string) error {
-    f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE |os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    CURRENT_LOGGER = log.New(f,"",log.LstdFlags)
-    return nil
+	f, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	CURRENT_LOGGER = log.New(f, "", log.LstdFlags)
+	return nil
 }
+
 /*
-    Amounts to SetOutput, which doesn't return err in std lib
+   Amounts to SetOutput, which doesn't return err in std lib
 */
 func SetLoggerUtc(filepath string) error {
-    dir := path.Dir(filepath)
-    err := syscall.Access(dir, syscall.O_RDWR)
-    if err != nil {
-        return err
-    }
-    /*
-    - erroring out on non existent filepaths 
-    - isn't expected behavior, as long as the dir exists and we
-    have permissions, should be fine
-    err = syscall.Access(filepath, syscall.O_RDWR)
-    if err != nil {
-        return err
-    }
-    */
-    UTC_LOGGER = new(LocalWriter)
-    UTC_LOGGER.DestLog = filepath
-    log.SetFlags(log.Lshortfile)
-    log.SetOutput(UTC_LOGGER)
-    return nil
+	dir := path.Dir(filepath)
+	if dir == "/" || filepath == "/" {
+		return errors.New("Shouldn't write to / ; try a different path")
+	}
+	err := syscall.Access(dir, syscall.O_RDWR)
+	if err != nil {
+		return err
+	}
+	UTC_LOGGER = new(LocalWriter)
+	UTC_LOGGER.DestLog = filepath
+	log.SetFlags(log.Lshortfile)
+	log.SetOutput(UTC_LOGGER)
+	return nil
 }
 
-
 func Inf(msg string) error {
-    if UTC_LOGGER != nil {
-        log.Println("[INFO] "+msg)
-        return nil
-    } else {
-        return errors.New(ERR_NIL_UTC_LOGGER)
-    }
+	if UTC_LOGGER != nil {
+		log.Println("[INFO] " + msg)
+		return nil
+	} else {
+		return errors.New(ERR_NIL_UTC_LOGGER)
+	}
 }
 
 func Wrn(msg string) error {
-    if UTC_LOGGER != nil {
-        log.Println("[WARN] "+msg)
-        return nil
-    } else {
-        return errors.New(ERR_NIL_UTC_LOGGER)
-    }
+	if UTC_LOGGER != nil {
+		log.Println("[WARN] " + msg)
+		return nil
+	} else {
+		return errors.New(ERR_NIL_UTC_LOGGER)
+	}
 }
 
 func Dbg(msg string) error {
-    if UTC_LOGGER != nil {
-        log.Println("[DEBUG] "+msg)
-        return nil
-    } else {
-        return errors.New(ERR_NIL_UTC_LOGGER)
-    }
+	if UTC_LOGGER != nil {
+		log.Println("[DEBUG] " + msg)
+		return nil
+	} else {
+		return errors.New(ERR_NIL_UTC_LOGGER)
+	}
 }
 
 func Err(e error) error {
-    if UTC_LOGGER != nil {
-        log.Println("[ERROR] "+e.Error())
-        return nil
-    } else {
-        return errors.New(ERR_NIL_UTC_LOGGER)
-    }
+	if UTC_LOGGER != nil {
+		log.Println("[ERROR] " + e.Error())
+		return nil
+	} else {
+		return errors.New(ERR_NIL_UTC_LOGGER)
+	}
 }
 
 /* Log with global logger after calling SetLogger */
 func Infos(msg string) {
-    if CURRENT_LOGGER !=  nil {
-        CURRENT_LOGGER.Println("INFO: "+msg)
-    } else {
-        panic(ERR_NIL_LOGGER)
-    }
+	if CURRENT_LOGGER != nil {
+		CURRENT_LOGGER.Println("INFO: " + msg)
+	} else {
+		panic(ERR_NIL_LOGGER)
+	}
 }
 func Warns(msg string) {
-    if CURRENT_LOGGER != nil {
-        CURRENT_LOGGER.Println("WARN: "+msg)
-    } else {
-        panic(ERR_NIL_LOGGER)
-    }
+	if CURRENT_LOGGER != nil {
+		CURRENT_LOGGER.Println("WARN: " + msg)
+	} else {
+		panic(ERR_NIL_LOGGER)
+	}
 }
 func Debugs(msg string) {
-    if CURRENT_LOGGER != nil {
-        CURRENT_LOGGER.Println("DEBUG: "+msg)
-    } else {
-        panic(ERR_NIL_LOGGER)
-    }
+	if CURRENT_LOGGER != nil {
+		CURRENT_LOGGER.Println("DEBUG: " + msg)
+	} else {
+		panic(ERR_NIL_LOGGER)
+	}
 }
 
 /* Log with pointer */
-func Info(someLogger *log.Logger,msg string) {
-    someLogger.Println("INFO: "+msg)
+func Info(someLogger *log.Logger, msg string) {
+	someLogger.Println("INFO: " + msg)
 }
 func Warn(someLogger *log.Logger, msg string) {
-    someLogger.Println("WARN: "+msg)
+	someLogger.Println("WARN: " + msg)
 }
 func Debug(someLogger *log.Logger, msg string) {
-    someLogger.Println("DBUG: "+msg)
+	someLogger.Println("DBUG: " + msg)
 }
-
--- tests/logger_test.go
+++ tests/logger_test.go
@@ -1,89 +1,105 @@
 package logger_test
 
-import "io"
-import "fmt"
-//import "bufio"
-import "errors"
-import "os"
-import "github.com/dukeofdisaster/simplelog/pkg/logger"
-//import "fmt"
-import "strings"
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"github.com/dukeofdisaster/simplelog/pkg/logger"
+	"io"
+	"os"
+	"strings"
+	"testing"
+	"time"
+)
+
 var badPaths = []struct {
-    // input
-    in string
-    // expected result
-    expected  []string
+	// input
+	in string
+	// expected result
+	expected []string
 }{
-    {"/noexist", []string{"read-only", "denied"}},
-    {"/", []string{"read-only","denied"}},
-    {"/noexist/noexist/test.log", []string{"no such"}},
-    {"/dev/stderr", []string{"no such"}},
+	{"/noexist", []string{"read-only", "denied", "Shouldn't"}},
+	{"/", []string{"read-only", "denied", "Shouldn't"}},
+	{"/noexist/noexist/test.log", []string{"no such"}},
 }
+
 func TestSetLoggerUtc_ShouldReturnErrs(t *testing.T) {
-    for _, testcase := range badPaths {
-        // all badPaths should generate an error
-        actual := logger.SetLoggerUtc(testcase.in)
-        // test case should contain at least one of the expected strings
-        good := 0
-        for _,s := range testcase.expected {
-            if strings.Contains(actual.Error(), s) {
-                good += 1
-            }
-        }
-        if good <  1  {
-            t.Errorf("Expected one of these strings: %s, in err: %v",testcase.expected,actual)
-        }
-        //fmt.Printf("good: %d\n",good)
-    }
+	for _, testcase := range badPaths {
+		// all badPaths should generate an error
+		actual := logger.SetLoggerUtc(testcase.in)
+		if actual == nil {
+			t.Errorf("expected SetLoggerUtc to return an error, but actual == nil")
+			return
+		}
+		// test case should contain at least one of the expected strings
+		good := 0
+		for _, s := range testcase.expected {
+			if strings.Contains(actual.Error(), s) {
+				good += 1
+			}
+		}
+		if good < 1 {
+			t.Errorf("Expected one of these strings: %s, in err: %v", testcase.expected, actual)
+		}
+	}
+}
+
+func TestSetLoggerUtc_InfWithBadPathShouldReturnErr(t *testing.T) {
+	for _, testcase := range badPaths {
+		err := logger.SetLoggerUtc(testcase.in)
+		if err == nil {
+			t.Errorf("Expected badpath from setlogger utc")
+			err = logger.Inf("DUDE")
+			if err == nil {
+				t.Errorf("Expected Inf() to return error but it didnt")
+			}
+		}
+	}
 }
 
 func TestInf_ValidInputWithNoSetLoggerUtcShouldReturnErr(t *testing.T) {
-    err := logger.Inf("test") 
-    if err == nil {
-        t.Errorf("Expected call to Inf() before SetLoggerUtc() to return error")
-    }
+	err := logger.Inf("test")
+	if err == nil {
+		t.Errorf("Expected call to Inf() before SetLoggerUtc() to return error")
+	}
 }
 
 func TestWrn_ValdidInputWithNoSetLoggerUtcShouldReturnErr(t *testing.T) {
-    err := logger.Wrn("test") 
-    if err == nil {
-        t.Errorf("Expected call to Wrn() before SetLoggerUtc() to return error")
-    }
+	err := logger.Wrn("test")
+	if err == nil {
+		t.Errorf("Expected call to Wrn() before SetLoggerUtc() to return error")
+	}
 }
 
 func TestDbg_ValdidInputWithNoSetLoggerUtcShouldReturnErr(t *testing.T) {
-    err := logger.Dbg("test") 
-    if err == nil {
-        t.Errorf("Expected call to Dbg() before SetLoggerUtc() to return error")
-    }
+	err := logger.Dbg("test")
+	if err == nil {
+		t.Errorf("Expected call to Dbg() before SetLoggerUtc() to return error")
+	}
 }
 
 func TestErr_ValdidInputWithNoSetLoggerUtcShouldReturnErr(t *testing.T) {
-    err := logger.Err(errors.New("test") )
-    if err == nil {
-        t.Errorf("Expected call to Err() before SetLoggerUtc() to return error")
-    }
+	err := logger.Err(errors.New("test"))
+	if err == nil {
+		t.Errorf("Expected call to Err() before SetLoggerUtc() to return error")
+	}
 }
 
-//func ExampleInf_ShouldContainInfInBrackets(t *testing.T) {
 func TestInf_ShouldContainInfInBrackets(t *testing.T) {
-    stdoutPath := "/dev/fd/2"
-    err := logger.SetLoggerUtc(stdoutPath)
-    if err != nil {
-        t.Errorf("unable to use val: %s, as path to stdout, got err: %v",stdoutPath,err)
-    }
-    logger.Inf("test stdout")
-
-    f, err := os.Open(stdoutPath)
-    defer f.Close()
-    if err != nil {
-        t.Errorf("unable to open stdout path as file")
-    }
-    buf_ten := make([]byte, 5)
-    num_read, err := io.ReadAtLeast(f,buf_ten,5)
-    fmt.Printf("%d bytes read: %s\n", num_read, string(buf_ten))
-    if string(buf_ten) == "dude" {
-        t.Errorf("expected bytes not in output: %s",buf_ten)
-    }
+	current_year := fmt.Sprintf("%v", time.Now().Year())
+	stdoutPath := "/tmp/simplelog.test"
+	err := logger.SetLoggerUtc(stdoutPath)
+	if err != nil {
+		t.Errorf("unable to use val: %s, as path to stdout, got err: %v", stdoutPath, err)
+	}
+	logger.Inf("test stdout")
+	f, err := os.Open(stdoutPath)
+	defer f.Close()
+	if err != nil {
+		t.Errorf("unable to open stdout path as file")
+	}
+	buf_four := make([]byte, 4)
+	_, err = io.ReadAtLeast(f, buf_four, 4)
+	if string(buf_four) != current_year {
+		t.Errorf("expected bytes not in output: %s", buf_four)
+	}
 }
